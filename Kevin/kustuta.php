c70dc15eb89cd7ddc270d08dd9c7308bd86ed0ac
kpajula <kevin_pajula@hotmail.com>
2018-05-30 14:14:27 +0300

    parandatud vead

--- Kevin/kustuta.php
+++ Kevin/kustuta.php
@@ -6,6 +6,7 @@ foreach($_POST['kustuta'] as $item){
 		if ($conn->query($sql) === TRUE) {
 		} else {
 			echo "Error: " . $sql . "<br>" . $conn->error;
+		}
 }
 header("Refresh:0; url=kontakt.php");
 $conn->close();
