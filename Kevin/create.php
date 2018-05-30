d7a424099f9a54d123d112b8d34c1fa4801275a0
kpajula <kevin_pajula@hotmail.com>
2018-05-30 13:50:24 +0300

    htmli uuendamine, kontaktivorm (php ja lokaalse andmebaasi jaoks koodi tegemine/andmete näitamine ei toimi hetkel)

---
+++ Kevin/create.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// Create database
+$sql = "CREATE DATABASE isikuandmed";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+
+
+header("Location: index.html");
+die();
+exit;
+
+?>
+
+
