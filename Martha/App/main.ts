651f05a9562b36404f9a9c9be9b313291d5f0ee9
T45\student <mmkikkas@gmail.com>
2018-06-06 14:47:13 +0300

    typescript veel

--- Martha/App/main.ts
+++ Martha/App/main.ts
@@ -1,3 +1,224 @@
 'use strict';
 
 console.log('Hello TS');
+/* Tõene ja väär */
+let kasTehtud: boolean = false; 
+/* Numbrid */
+const kymmend: number = 6;
+const hwx = 0xfd02;
+const binaar = 0b1010;
+let octal = 0o732;
+octal = 7;
+
+/* sõnad */
+let lause: string = 'Tere tulemast ' + 'koolitusele.\n' 
+                  + 'Täna meil programmeerimine';
+let vanus = 29;
+let minuVanusLauses =
+     `Minu vanus on ${vanus}.
+     Tere Tulemast.`;
+
+/* massiivid */
+const massiiv: number[] = [1, 4 , 7];
+const massiiv2: Array<number> = [1, 5, 9];
+const arv= massiiv[2]; // 7
+
+
+/* tuple */
+const tuple: [string, number] = ['Tere ', 15];
+const s6ne = tuple[0];
+/* loendajad, enum */
+enum V2rv {Punane, Roheline, Sinine}
+const v2rv: V2rv = V2rv.Sinine;
+
+/* any_void */
+const poleKindel: any = 'Tere';
+poleKindel = 5;
+const nimekiri: any[] = [2, 'Hi', 5.32];
+nimekiri[0] = 'tere';
+const tervitus = nimekiri[1] as string;
+let muutuja = nimekiri[0] = 'Tere';
+
+function funktsioon(): void {
+     console.log('hello function');
+
+     return;
+ }
+
+ /* null undefined */
+ let un: string;
+ const u:undefined = undefined;
+ const n:number = null;
+ 
+ /* never - kunagi ei saa funktsiooni lõpuni */
+
+ function l6putuTsykkel(): never {
+     while (true) {console.log('never fun'); }
+ }
+
+ /* object - kõik ülejäänud peale eelmiste */ 
+const obj: object = l6putuTsykkel;
+const obj2: object = {nimi: 'Juku', vanus: 25};
+
+/* var (let on parem, sarnasem teiste keeltega) let const */
+function varLet() {
+    var variable = 17;
+    var variable2 = 17;
+    if (true) {
+        var variable = 27;
+        var variable2 = 27;
+}
+        console.log(variable); // 27undefined
+        console.log(variable2); // 17error
+}
+    for (var i = 0; i < 10; i++) {
+        setTimeout(function() { console.log(i); }, 200);
+    }
+
+    for (let i = 0; i < 10; i++) {
+        setTimeout(function() { console.log(i); }, 200);
+    }
+    // for (var i_1 = 0; i_1 < 10; i_1++) {
+    //    _loop_1(i_1);
+//varLet();
+
+/*Destrukteerimine*/
+
+const sisend = [1, 2, 3, 4, 5];
+//const [a, b] = sisend;
+//const [c] = sisend;
+const [esi, teine, ...ylej22nud] = sisend;
+//console.log(a + b);
+//console.log(c);
+//console.log(ylej22nud);
+console.log(`${esi} ${teine} ${ylej22nud}`);
+const o = {
+    a: 'ssds',
+    b: 1324 ,
+    c: 'kar' ,
+    d: 134
+
+};
+
+const kar = o.c;
+const {a, b, ...l6pp} = o;
+console.log(a + b + l6pp.d);
+
+function hoiaObjektAlles(terveObjekt: {x: string, y?: number }) {
+    const {x, y = 2018} = terveObjekt;
+    console.log(x+y);
+}
+hoiaObjektAlles({x: 'Tere ', y: 2017});
+
+/*spread (erinevad operaatorid= plussid miinused, punktid jne*/
+
+const mass = [1, 2];
+const mass2 = [3, 4];
+const suurMass = [1, ...mass, ...mass2, 5];
+
+const vaikeV22rtused = {
+    toit: 'Terav', 
+    hind: 'Kõrvetav',
+    keskkond: 'Rahulik'
+};
+
+const maheKohvik = {
+    ...vaikeV22rtused, 
+    toit: 'Mahe'
+};
+
+/* interface */ 
+
+interface IRuut { 
+    
+    x: number; 
+
+    y: number;   
+}
+
+interface IKuup extends IRuut {
+    z?: number;
+}
+function saaAla({x, y, z = 1}: IKuup) {
+    console.log(x * y * z);
+}
+const ruut: IRuut= {x: 12, y: 15};
+// ruut.x = 134;
+ruut.y = 10; 
+saaAla(ruut);
+
+interface IMiniFunk{
+    [index: number]: string;
+    (sisend: string): boolean;
+}
+const miniFunk: MiniFunk = (sisend: string) => false ;
+
+interface IMiniArray{
+    [index: number]: string;
+  
+}
+const minimassiiv: MiniArray = ["Juku", 'Kalle'];
+const juku = minimassiiv[0];
+
+/* funktsioonid*/
+
+function summa(arv1:string, arv2:string): string;
+function summa(arv1:number, arv2:number): number;
+function summa(arv1:any, arv2:any):any {
+        return arv1 + arv2;
+}
+console.log('Tere', ' pere!'));
+console.log(summa(3,4)); // 7
+console.log(summa()); // 6
+const minuSumma: (arv1: number, arv2: number) => number
+ = function(arv1: number, arv2: number) {return arv1 + arv2; };
+console.log(minuSumma(3,4)); //7
+(function(arv1: number, arv2: number) {return arv1 + arv2; })(3,4); //7
+const minuSumma2 = (arv1 = 2, arv2 = 7) => arv1 + arv2);
+console.log(minuSumma2(3, 4)); //7
+console.log(minuSumma2(9)); //7
+
+/* types */
+
+type Funktsioon = (sisend: string) => boolean;
+
+type T2rn = 1 | 2 | 3 | 4 | 5 | 6;
+const visatudT2rn: T2rn = 4; 
+
+type Suvaline = 2 | "Tere" | false | undefined | null;
+const suvaline: Suvaline = "Tere";
+
+/* m2rk !== m2rk2 */
+const m2rk: symbol = Symbol("key");
+const m2rk2: symbol = Symbol("key");
+if(m2rk === m2rk2){
+        console.log('Esimene');
+}else if (m2rk == m2rk2) {
+    console.log('teine');
+}else {
+    console.log('Muu');
+}
+
+/* if else switch */
+/* === == !== != < > <= => */
+/* || && */ 
+const arv3 = 26;
+const arv4 = 43;
+
+if ( (arv3 + arv4 > 30) )  {
+    console.log('Esimene');
+} else if ( 2 + 2 === 3) {
+    console.log('2 + 2 === 3');
+} else {
+    console.log('Kumbki ei sobinud');
+}
+
+const muutujad = arv3 < arv4 ?  true : false
+
+
+
+
+
+
+
+
