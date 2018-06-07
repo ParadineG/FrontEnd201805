fa1ed8949b59dfbdd94166f79efa29988690457a
georg <georg.tammeleht@gmail.com>
2018-06-07 14:56:16 +0300

    Veel TypeScripti

---
+++ Georg/App/nimeroom.ts
@@ -0,0 +1,13 @@
+'use strict'
+console.log('nimeruum.ts')
+
+namespace Nimeruum {
+    console.log('in namespace');
+    let funkNimetus = 'funkNimetus'
+    export function funkar() {
+        console.log('Funkar');
+    }
+}
+function funkar2() {
+    console.log('Funkar2');
+}
