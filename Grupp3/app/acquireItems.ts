b90e84d03b0d109db9da83df46027960eb23d2ce
Ott Kossar <ott.kossar@gmail.com>
2018-06-11 14:44:22 +0300

    Grupp 3

--- Grupp3/app/acquireItems.ts
+++ Grupp3/app/acquireItems.ts
@@ -34,14 +34,22 @@ class AcquireItems extends Page {
         
         if (this._module && this._template) {
             this._module.outerHTML = this._template;
+            this._module = document.querySelector('main');
         }
         this._bindEvents();
         this._render();
     }
     protected _bindEvents(){
-
+        // tyhi
     }
-    protected _render(){
-
+    protected async _render(){
+        const data = await Helper.fetchContent('/data/featuredPosts.php');
+        if(data) {
+            this._posts = JSON.parse(data) as IPost[];
+            if (this._module) {
+                this._module.innerText = this._posts[0].description;
+            }
+        }
+       
     }
 }
